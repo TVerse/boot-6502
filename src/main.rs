496025c3b80e28fe74a1b1275840e59815e86b9d
Tim Evers <29369097+TVerse@users.noreply.github.com>
2023-01-16 21:34:47 +0100

    Rust echo half

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,46 @@
+use std::time::Duration;
 use anyhow::anyhow;
 use anyhow::Result;
 
-use boot_6502::get_default_serial;
+use boot_6502::{Frame, FrameDeserializer, get_default_serial, Payload};
 use tokio_serial::SerialStream;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::select;
+use tokio::time::sleep;
 
-const MSG: &[u8] = b"xHello!\0";
+const MSG: &[u8] = b"Hello!";
 
 #[tokio::main]
 async fn main() -> Result<()> {
     println!("Opening serial port");
     let mut serial = get_default_serial()?;
 
-    let res = ping(&mut serial).await;
+    let res = echo(&mut serial).await;
     serial.flush().await?;
     res
 }
 
-async fn ping(serial: &mut SerialStream) -> Result<()> {
+async fn echo(serial: &mut SerialStream) -> Result<()> {
+    let mut deserializer = FrameDeserializer::default();
     loop {
-        println!("Waiting for signal");
-        serial.flush().await?;
-        let mut rcvd = [0; 1];
-        serial.read_exact(&mut rcvd).await?;
-        if rcvd != [0x55] {
-            return Err(anyhow!("Wrong init byte, got {rcvd:?}"));
-        }
-        println!("Generating and sending");
-        serial.write_all(MSG).await?;
-        println!("Flushing");
-        serial.flush().await?;
-        let mut rcvd = [0; MSG.len()];
-        println!("Reading");
-        serial.read_exact(&mut rcvd).await?;
-        if rcvd == MSG {
-            println!(
-                "Success! Got {:?}, {}",
-                &rcvd,
-                String::from_utf8_lossy(&rcvd)
-            );
-        } else {
-            println!("Got {:?}, {}", &rcvd, String::from_utf8_lossy(&rcvd));
+        let frame = Frame::new(Payload::Echo(MSG.to_vec()).serialize());
+        serial.write(&frame.serialize()).await?;
+        let sleep = sleep(Duration::from_millis(500));
+        tokio::pin!(sleep);
+        let mut buf = Vec::new();
+        let b = select! {
+            r = serial.read_u8() => r?,
+            _ = &mut sleep => {
+                println!("No response");
+                continue
+            },
+        };
+        buf.push(b);
+        let out_frame = deserializer.push(b);
+        println!("Raw buf: {:?}", &buf);
+        if let Some(f) = out_frame {
+            println!("Frame returned: {f:?}");
+            buf.clear();
         }
     }
 }
